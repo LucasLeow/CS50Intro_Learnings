1a177c34be3d46c2551a356577f489372b6190fd
Lucas Leow <leowjianhoong@gmail.com>
2023-06-06 16:50:38 +0800

    Completed Wk 1 Lecture

---
+++ code/Week1/cs50_wk1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+    int num;
+
+    printf("Enter an integer: ");
+    num = scanf("%d", &num);
+
+    printf("%d", num);
+    return 0;
+
+}
